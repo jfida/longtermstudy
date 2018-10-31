e9af7e07495417955e0c5d4687f4193143e42bc3
Bianca Stancu <bianca.stancu28@gmail.com>
2018-10-31 19:02:49 +0100

    Add Activity Alert Dialog

--- app/src/main/java/usi/memotion/UI/fragments/EdiaryFragment.java
+++ app/src/main/java/usi/memotion/UI/fragments/EdiaryFragment.java
@@ -1,7 +1,10 @@
 package usi.memotion.UI.fragments;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,10 +20,38 @@ public class EdiaryFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(final LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ediary, container, false);
+        View view = inflater.inflate(R.layout.fragment_ediary, container, false);
+
+        FloatingActionButton fab = (FloatingActionButton) view.findViewById(R.id.add_activity_button);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final AlertDialog.Builder builderAdd = new AlertDialog.Builder(getContext());
+
+                builderAdd.setCancelable(false);
+                builderAdd.setView(inflater.inflate(R.layout.create_activity_layout, null));
+                builderAdd.setPositiveButton("SUBMIT", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+                builderAdd.setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+                builderAdd.create();
+                builderAdd.show();
+            }
+
+            ;
+        });
+        return view;
     }
 
 }
